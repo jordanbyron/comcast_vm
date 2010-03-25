db4629efac3b628dd1a06834024914615650760a
Jordan Byron <jordan@ducksoupsoftware.com>
2010-03-25 19:23:15 -0400

    Begin loading vms

--- lib/comcast_vm/digital_voice.rb
+++ lib/comcast_vm/digital_voice.rb
@@ -1,5 +1,7 @@
 module ComcastVm
   class DigitalVoice
+    attr_reader :main_page, :voicemail
+    
     def initialize(phone, pin)
       @agent = WWW::Mechanize.new { |agent| agent.follow_meta_refresh = true }
       @url   = "https://digitalvoice.comcast.net/"
@@ -7,6 +9,7 @@ module ComcastVm
       @pin   = pin
 
       login
+      find_voicemail
     end
 
     def voicemail?
@@ -14,7 +17,8 @@ module ComcastVm
     end
 
     def voicemail_count
-      @main_page.search("//a[@id='Voice Mail']").first.parent.parent.search("span.right").text.strip.to_i
+      @voicemail.length
+      #@main_page.search("//a[@id='Voice Mail']").first.parent.parent.search("span.right").text.strip.to_i
     end
     
     private
@@ -28,6 +32,15 @@ module ComcastVm
       login_form.dvPin      = @pin
 
       @main_page = @agent.submit(login_form, login_form.buttons.first)
+    end
+    
+    def find_voicemail
+      @voicemail = []
+      
+      @main_page.search("div.dashboard").each do |voicemail|
+        number = voicemail.children[3].text.strip
+        @voicemail << Voicemail.new(number)
+      end
     end
   end
 end
--- lib/comcast_vm/voicemail.rb
+++ lib/comcast_vm/voicemail.rb
@@ -1,5 +1,9 @@
 module ComcastVm
   class Voicemail
+    attr_reader :number
     
+    def initialize(number)
+      @number = number
+    end
   end
 end
